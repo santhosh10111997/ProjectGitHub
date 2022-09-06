a8f512bc3a9acb6602c6380eaa2ab109c3613f42
santhosh10111997 <sandytambasandy@gmail.com>
2022-09-06 12:46:54 +0530

    santhosh's code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -2,4 +2,10 @@ package org.test;
 
 public class Sample {
 
+	public static void main(String[] args) {
+		
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+	}
 }
